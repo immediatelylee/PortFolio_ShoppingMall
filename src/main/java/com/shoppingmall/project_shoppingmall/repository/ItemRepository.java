2c4b3faca07fc64e84ad85cdb0e36aa2a556dfbb
commit name <dydgh1095@naver.com>
2023-08-19 23:29:56 +0900

    #1 - ItemRepository 구현
    @Query 어노테이션과 nativeQuery로도 구현을 해 보았다.

---
+++ src/main/java/com/shoppingmall/project_shoppingmall/repository/ItemRepository.java
@@ -0,0 +1,30 @@
+package com.shoppingmall.project_shoppingmall.repository;
+
+import com.shoppingmall.project_shoppingmall.domain.*;
+import org.springframework.data.jpa.repository.*;
+import org.springframework.data.querydsl.*;
+import org.springframework.data.repository.query.*;
+
+import java.util.*;
+
+public interface ItemRepository extends JpaRepository<Item, Long>, QuerydslPredicateExecutor<Item> {
+
+    List<Item> findByItemNm(String itemNm);
+
+    List<Item> findByItemNmOrItemDetail(String itemNm, String itemDetail);
+
+    List<Item> findByPriceLessThan(Integer price);
+
+    List<Item> findByPriceLessThanOrderByPriceDesc(Integer price);
+
+    @Query("select i from Item i where i.itemDetail like " +
+            "%:itemDetail% order by i.price desc")
+    List<Item> findByItemDetail(@Param("itemDetail") String itemDetail);
+
+    @Query(value="select * from item i where i.item_detail like " +
+            "%:itemDetail% order by i.price desc", nativeQuery = true)
+    List<Item> findByItemDetailByNative(@Param("itemDetail") String itemDetail);
+
+
+}
+
