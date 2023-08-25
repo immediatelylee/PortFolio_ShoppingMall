e14d6928404d98dd7daa4914084bc2b0ecd6e630
commit name <dydgh1095@naver.com>
2023-08-25 18:08:16 +0900

    #10 - 상품 이미지 저장 서비스 구현
    - ItemServiceTest   MockMultipartFile클래스를 이용해서 mocking된 file을 만들어 반환하였음.
    상품데이터와 이미지 정보를 파라미터로 넘겨서 저장하고 입력상품과 실제 상품이 같은지 확인하였음.
    - ItemImgRepository   파일 이미지 저장
    - ItemImgService 상품 이미지 정보를 저장하는 service     환경변수를 불러와서 어디에 저장할지 어디서 불러올지를 지정
    - ItemService   ItemImgService와 함께 상품 정보를 저장하는 서비스

---
+++ src/main/java/com/shoppingmall/project_shoppingmall/service/ItemService.java
@@ -0,0 +1,51 @@
+package com.shoppingmall.project_shoppingmall.service;
+
+import com.shoppingmall.project_shoppingmall.domain.*;
+import com.shoppingmall.project_shoppingmall.dto.*;
+import com.shoppingmall.project_shoppingmall.repository.*;
+import lombok.*;
+import org.springframework.data.domain.*;
+import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
+import org.springframework.web.multipart.*;
+
+import javax.persistence.*;
+import java.util.*;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    private final ItemImgService itemImgService;
+
+    private final ItemImgRepository itemImgRepository;
+
+    public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
+
+        //상품 등록
+        Item item = itemFormDto.createItem();
+        itemRepository.save(item);
+
+        //이미지 등록
+        for(int i=0;i<itemImgFileList.size();i++){
+            ItemImg itemImg = new ItemImg();
+            itemImg.setItem(item);
+
+            if(i == 0)
+                itemImg.setRepimgYn("Y");
+            else
+                itemImg.setRepimgYn("N");
+
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+
+        return item.getId();
+    }
+
+//
+
+}
+
