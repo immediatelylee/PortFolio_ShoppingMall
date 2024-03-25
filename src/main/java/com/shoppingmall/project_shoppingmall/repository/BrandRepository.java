955ea0f6d8358a5b133c1d2d9a97bc2676811826
commit name <dydgh1095@naver.com>
2024-03-25 11:02:16 +0900

    #29 BrandRepository에 추가한요소(querydsl 상속, custom상속,findByBrandNm)

--- src/main/java/com/shoppingmall/project_shoppingmall/repository/BrandRepository.java
+++ src/main/java/com/shoppingmall/project_shoppingmall/repository/BrandRepository.java
@@ -2,6 +2,13 @@ package com.shoppingmall.project_shoppingmall.repository;
 
 import com.shoppingmall.project_shoppingmall.domain.*;
 import org.springframework.data.jpa.repository.*;
+import org.springframework.data.querydsl.*;
 
-public interface BrandRepository extends JpaRepository<Brand,Long> {
+import java.util.*;
+
+public interface BrandRepository extends JpaRepository<Brand,Long> ,
+        QuerydslPredicateExecutor<Brand>,BrandRepositoryCustom {
+
+    List<Brand> findByBrandNm(String brandNm);
+
 }
