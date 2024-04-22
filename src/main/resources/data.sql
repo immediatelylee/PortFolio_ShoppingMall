188b9872f2d567d8cfdfc3b84d0e35a0905fcac8
commit name <dydgh1095@naver.com>
2024-04-22 20:29:16 +0900

    #33 - 유저 input 주석처리
    create -> validate로인하여 주석처리 하였음.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO member (name, email, password, address, role)
-VALUES ('John Doe', 'admin@example.com', '$2a$12$NiP8ZlFx6bcXIvXwWy0BtOEu2ZWbg4IfHGaUJhB6d.Epygv5Xba5q', '123 Main St', 'ADMIN');
-INSERT INTO member (name, email, password, address, role)
-VALUES ('User', 'user@example.com', '$2a$12$NiP8ZlFx6bcXIvXwWy0BtOEu2ZWbg4IfHGaUJhB6d.Epygv5Xba5q', '123 Main St', 'USER');
+# INSERT INTO member (name, email, password, address, role)
+# VALUES ('John Doe', 'admin@example.com', '$2a$12$NiP8ZlFx6bcXIvXwWy0BtOEu2ZWbg4IfHGaUJhB6d.Epygv5Xba5q', '123 Main St', 'ADMIN');
+# INSERT INTO member (name, email, password, address, role)
+# VALUES ('User', 'user@example.com', '$2a$12$NiP8ZlFx6bcXIvXwWy0BtOEu2ZWbg4IfHGaUJhB6d.Epygv5Xba5q', '123 Main St', 'USER');
