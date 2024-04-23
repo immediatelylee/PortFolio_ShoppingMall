2af0081b4a2e9fa5933ccf9d93802f3253a32018
commit name <dydgh1095@naver.com>
2024-04-24 01:49:42 +0900

    #33 - 컨트롤러에 페이지네이션 서비스 추가하고 타입별 검색이 가능하게 수정
    - 이거 구현하면서 다시한번 @RequestParam을 이용하여 검색기능에 써먹는 방법과 @PageableDefault가 pageable이 없을때 디폴트를 정해준다는 사실을 다시 알게됨
    
    - 타입별 검색이 쉬운거 같은데 thymeleaf로 하려니 잘 안되어서 게속 controller, service에서 헤매다가 시간 좀 많이 날렸음 그만큼 뒤적거리다가 모르는 사실도 많이 알게 됨.

--- src/main/java/com/shoppingmall/project_shoppingmall/controller/BrandController.java
+++ src/main/java/com/shoppingmall/project_shoppingmall/controller/BrandController.java
@@ -6,11 +6,14 @@ import com.shoppingmall.project_shoppingmall.dto.*;
 import com.shoppingmall.project_shoppingmall.service.*;
 import lombok.*;
 import org.springframework.beans.factory.annotation.*;
+import org.springframework.data.domain.*;
+import org.springframework.data.web.*;
 import org.springframework.stereotype.*;
 import org.springframework.ui.*;
 import org.springframework.validation.*;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.validation.*;
 import java.util.*;
@@ -21,17 +24,16 @@ import java.util.stream.*;
 public class BrandController {
 
     private final BrandService brandService;
+    private final PaginationService paginationService;
 
     @ModelAttribute("brandStatus")
     public BrandStatus[] brandStatuses(){
         return BrandStatus.values();
     }
 
-    @ModelAttribute("brands")
-    public List<Brand> brandList() {
-        return brandService.findAll();
-    }
-
+//    @ModelAttribute("brands")
+//    public Page<Brand> brandList(Pageable pageable) { return brandService.findAll(pageable);
+//    }
 
     @GetMapping("/admin/brand/brandadd")
     public String brandForm(@ModelAttribute("brandFormDto") BrandFormDto brandFormDto){
@@ -42,7 +44,6 @@ public class BrandController {
     @PostMapping("/admin/brand/brandadd")
     public String savebrand(@Valid BrandFormDto brandFormDto ,
                             BindingResult bindingResult, Model model){
-
         if(bindingResult.hasErrors()){
 
             return "brand/brandForm";
@@ -50,24 +51,43 @@ public class BrandController {
 
         try {
             brandService.saveBrand(brandFormDto);
-
             model.addAttribute("message", "브랜드가 등록되었습니다.");
             return "redirect:/admin/brand/management";
-
 
         } catch (Exception e){
             model.addAttribute("errorMessage", "브렌드 등록에 실패했습니다.");
             return "brand/brandForm";
         }
-//        return "brand/brandForm";
+
     }
 
     @GetMapping(value = "/admin/brand/management")
-    public String brandmanage(Model model){
-        model.addAttribute("brandFormDto",new BrandFormDto());
+    public String brandmanage(@RequestParam(required = false) BrandSearchType brandSearchType,
+                              @RequestParam(required = false) String searchValue,
+                              @RequestParam(required = false) String searchStatus,
+                              @PageableDefault(page = 0,size = 10) Pageable pageable,
+                              Model model){
+
+        Page<Brand> searchBrands = brandService.searchBrands(brandSearchType,searchValue,searchStatus,pageable).map(BrandFormDto::toBrand);
+        List<Integer> barNumbers = paginationService.getPaginationBarNumbers(pageable.getPageNumber(),searchBrands.getTotalPages());
+
+
+        model.addAttribute("paginationBarNumbers", barNumbers);
+        model.addAttribute("BrandSearchType",brandSearchType.values());
         model.addAttribute("IdsTransferDto",new IdsTransferDto());  // retrun list
+        model.addAttribute("searchBrands",searchBrands);
         return "brand/brandForm";
     }
+
+//    @GetMapping(value = "/admin/brand/management/{brandId}")
+//    public String brand(@PathVariable Long brandId,Model model){
+//        Brand brand = brandService.getBrand(brandId);
+//        model.addAttribute("brand",brand);
+//
+//
+//    return "brand/brandAdd";
+//    }
+
 
     @PostMapping("/admin/brand/deleteBrands")
     public String deleteBrands(IdsTransferDto idsTransferDto){
