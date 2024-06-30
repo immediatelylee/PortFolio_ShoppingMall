cb290d4e4861c09cc5a6c5dfe4b9112906d66fb4
commit name <dydgh1095@naver.com>
2024-06-30 22:41:37 +0900

    #45 - 테스트를 위한 mainItemDto
    이전에 쓰던것으로 추후에 삭제 예정

--- src/main/java/com/shoppingmall/project_shoppingmall/dto/MainItemDto.java
+++ src/main/java/com/shoppingmall/project_shoppingmall/dto/MainItemDto.java
@@ -1,6 +1,7 @@
 package com.shoppingmall.project_shoppingmall.dto;
 
 import com.querydsl.core.annotations.*;
+import com.shoppingmall.project_shoppingmall.constant.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,20 +9,23 @@ import lombok.Setter;
 public class MainItemDto {
 
     private Long id;
-
+    private String itemCode;
     private String itemNm;
 
-    private String itemDetail;
+    private ItemSellStatus itemSellStatus;
 
+    private ItemDisplayStatus itemDisplayStatus;
     private String imgUrl;
 
     private Integer price;
 
     @QueryProjection
-    public MainItemDto(Long id, String itemNm, String itemDetail, String imgUrl,Integer price){
+    public MainItemDto(Long id,String itemCode, String itemNm,ItemSellStatus itemSellStatus,ItemDisplayStatus itemDisplayStatus , String imgUrl,Integer price){
         this.id = id;
+        this.itemCode = itemCode;
         this.itemNm = itemNm;
-        this.itemDetail = itemDetail;
+        this.itemDisplayStatus = itemDisplayStatus;
+        this.itemSellStatus = itemSellStatus;
         this.imgUrl = imgUrl;
         this.price = price;
     }
