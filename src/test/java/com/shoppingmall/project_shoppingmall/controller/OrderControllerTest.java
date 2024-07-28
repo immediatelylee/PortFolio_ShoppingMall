ad339089ada66159a7a6cf7605d9d3c79a713ee3
commit name <dydgh1095@naver.com>
2024-07-28 18:44:59 +0900

    #53-딱히 불필요한 커밋
    혹시나 해서 변경사항으로 저장

---
+++ src/test/java/com/shoppingmall/project_shoppingmall/controller/OrderControllerTest.java
@@ -0,0 +1,15 @@
+package com.shoppingmall.project_shoppingmall.controller;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.*;
+import org.springframework.boot.test.context.*;
+import org.springframework.test.context.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource(locations="classpath:application-test.properties")
+class OrderControllerTest {
+
+
+}
