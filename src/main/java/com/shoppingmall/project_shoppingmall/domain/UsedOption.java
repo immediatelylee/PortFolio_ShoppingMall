11ad83a6cab231a58fc759c0b166691d472c0a10
lee <dydgh1095@naver.com>
2024-12-26 01:37:55 +0900

    #67- g/setter추가

--- src/main/java/com/shoppingmall/project_shoppingmall/domain/UsedOption.java
+++ src/main/java/com/shoppingmall/project_shoppingmall/domain/UsedOption.java
@@ -1,8 +1,13 @@
 package com.shoppingmall.project_shoppingmall.domain;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 public class UsedOption {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
