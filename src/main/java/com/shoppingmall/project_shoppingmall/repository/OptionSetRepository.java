a8ae4c04752ebef42ac21150d75b11226717e726
lee <dydgh1095@naver.com>
2024-12-26 01:04:54 +0900

    #67- repository추가

---
+++ src/main/java/com/shoppingmall/project_shoppingmall/repository/OptionSetRepository.java
@@ -0,0 +1,7 @@
+package com.shoppingmall.project_shoppingmall.repository;
+
+import com.shoppingmall.project_shoppingmall.domain.OptionSet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public class OptionSetRepository extends JpaRepository<OptionSet,Long> {
+}
---
+++ src/main/java/com/shoppingmall/project_shoppingmall/repository/UsedOptionRepository.java
@@ -0,0 +1,7 @@
+package com.shoppingmall.project_shoppingmall.repository;
+
+import com.shoppingmall.project_shoppingmall.domain.UsedOption;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsedOptionRepository extends JpaRepository<UsedOption,Long> {
+}
