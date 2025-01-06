c1ed50b1e3b870edff3e710591947395557957ad
lee <dydgh1095@naver.com>
2025-01-06 17:52:56 +0900

    #67-조합일치형,분리형 enum 구분

---
+++ src/main/java/com/shoppingmall/project_shoppingmall/constant/OptionDisplayType.java
@@ -0,0 +1,6 @@
+package com.shoppingmall.project_shoppingmall.constant;
+
+public enum OptionDisplayType {
+    COMBINED,   // 조합 일체형 -> OptionCombination 활용
+    SEPARATED   // 분리 선택형 -> Option / OptionValue(또는 UsedOption) 사용
+}
