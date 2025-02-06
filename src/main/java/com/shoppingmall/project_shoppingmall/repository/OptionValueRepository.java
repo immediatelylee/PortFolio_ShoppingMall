44f220e0adbc0f93977c6669f351a7c0448f73f7
lee <dydgh1095@naver.com>
2025-02-06 18:20:32 +0900

    #67 - 미리 생성

---
+++ src/main/java/com/shoppingmall/project_shoppingmall/repository/OptionValueRepository.java
@@ -0,0 +1,7 @@
+package com.shoppingmall.project_shoppingmall.repository;
+
+import com.shoppingmall.project_shoppingmall.domain.OptionValue;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionValueRepository extends JpaRepository<OptionValue,Long> {
+}
