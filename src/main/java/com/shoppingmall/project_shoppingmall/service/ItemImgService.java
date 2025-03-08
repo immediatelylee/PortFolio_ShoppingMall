5d6128dc4544fc658c7a843445de2fb728f5d23f
lee <dydgh1095@naver.com>
2025-03-08 14:25:38 +0900

    이미지 경로 설정 수정
    리눅스와 윈도우 둘다 사용하다보니 이미지 경로를 못찾아서 에러가 나는 경우가 생김
    uri형태로 변환후 기존의 itemImgLocation부분을 basePath로 변경하였음.

--- src/main/java/com/shoppingmall/project_shoppingmall/service/ItemImgService.java
+++ src/main/java/com/shoppingmall/project_shoppingmall/service/ItemImgService.java
@@ -9,6 +9,10 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 import org.thymeleaf.util.StringUtils;
 import javax.persistence.EntityNotFoundException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 @Service
@@ -25,6 +29,18 @@ public class ItemImgService {
 
     private final FileService fileService;
 
+    /**
+     * itemImgLocation에 설정된 URI 형식의 문자열을 올바른 Path 객체로 변환
+     */
+    private Path getItemImgDirectory() {
+        try {
+            URI uri = new URI(itemImgLocation);
+            return Paths.get(uri);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("itemImgLocation 값의 URI 형식이 올바르지 않습니다: " + itemImgLocation, e);
+        }
+    }
+
     public void saveItemImg(ItemImg itemImg, MultipartFile itemImgFile) throws Exception {
         String oriImgName = itemImgFile.getOriginalFilename();
         String imgName = "";
@@ -32,7 +48,8 @@ public class ItemImgService {
 
         //파일 업로드
         if (!StringUtils.isEmpty(oriImgName)) {
-            imgName = fileService.uploadFile(itemImgLocation, oriImgName,
+            String basePath = getItemImgDirectory().toString();
+            imgName = fileService.uploadFile(basePath, oriImgName,
                     itemImgFile.getBytes());
             imgUrl = "/images/item/" + imgName;
         }
@@ -48,7 +65,8 @@ public class ItemImgService {
 
         //파일 업로드
         if (!StringUtils.isEmpty(oriImgName)) {
-            imgName = fileService.createThumbnail(itemImgLocation, oriImgName,
+            String basePath = getItemImgDirectory().toString();
+            imgName = fileService.createThumbnail(basePath, oriImgName,
                     itemImgFile.getBytes());
             imgUrl = "/images/item/" + imgName;
         }
@@ -65,7 +83,8 @@ public class ItemImgService {
 
         //파일 업로드
         if (!StringUtils.isEmpty(oriImgName)) {
-            imgName = fileService.uploadFile(itemImgLocation, oriImgName,
+            String basePath = getItemImgDirectory().toString();
+            imgName = fileService.uploadFile(basePath, oriImgName,
                     itemDetailFile.getBytes());
             imgUrl = "/images/item/" + imgName;
         }
@@ -75,28 +94,28 @@ public class ItemImgService {
 
     }
 
-    //상품 수정
+    // TODO: 하위 메소드들이 사용되고 있지 않는데 상품수정에서 문제가 없는지 확인하고 제거
+    // 상품 수정: 이미지 업데이트
     public void updateItemImg(Long itemImgId, MultipartFile itemImgFile) throws Exception {
         if (!itemImgFile.isEmpty()) {
             ItemImg savedItemImg = itemImgRepository.findById(itemImgId)
                     .orElseThrow(EntityNotFoundException::new);
 
-            //기존 이미지 파일 삭제
+            // 올바른 basePath를 구함
+            String basePath = getItemImgDirectory().toString();
+
+            // 기존 이미지 파일 삭제
             if (!StringUtils.isEmpty(savedItemImg.getImgName())) {
-                fileService.deleteFile(itemImgLocation + "/" +
-                        savedItemImg.getImgName());
+                fileService.deleteFile(basePath + "/" + savedItemImg.getImgName());
             }
 
-
-
             String oriImgName = itemImgFile.getOriginalFilename();
-            String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemImgFile.getBytes());
+            String imgName = fileService.uploadFile(basePath, oriImgName, itemImgFile.getBytes());
             String imgUrl = "/images/item/" + imgName;
             savedItemImg.updateItemImg(oriImgName, imgName, imgUrl);
 
-            itemImgRepository.save(savedItemImg);// 변경사항 저장
+            itemImgRepository.save(savedItemImg); // 변경사항 저장
         }
-
     }
 
     public void updateItemDetailImg(Long itemDetailImgId, MultipartFile itemDetailImgFile) throws Exception {
@@ -104,40 +123,43 @@ public class ItemImgService {
             ItemDetailImg savedItemDetailImg = itemDetailImgRepository.findById(itemDetailImgId)
                     .orElseThrow(EntityNotFoundException::new);
 
-            //기존 이미지 파일 삭제
+            // 올바른 basePath를 구함
+            String basePath = getItemImgDirectory().toString();
+
+            // 기존 이미지 파일 삭제
             if (!StringUtils.isEmpty(savedItemDetailImg.getImgName())) {
-                fileService.deleteFile(itemImgLocation + "/" +
-                        savedItemDetailImg.getImgName());
+                fileService.deleteFile(basePath + "/" + savedItemDetailImg.getImgName());
             }
 
             String oriImgName = itemDetailImgFile.getOriginalFilename();
-            String imgName = fileService.uploadFile(itemImgLocation, oriImgName, itemDetailImgFile.getBytes());
+            String imgName = fileService.uploadFile(basePath, oriImgName, itemDetailImgFile.getBytes());
             String imgUrl = "/images/item/" + imgName;
             savedItemDetailImg.updateItemDetailImg(oriImgName, imgName, imgUrl);
 
             itemDetailImgRepository.save(savedItemDetailImg);
         }
-
     }
+
     public void updateItemThumbnail(Long itemThumbnailId, MultipartFile itemThumbnailFile) throws Exception {
         if (!itemThumbnailFile.isEmpty()) {
             ItemThumbnail savedItemThumbnail = itemThumbnailRepository.findById(itemThumbnailId)
                     .orElseThrow(EntityNotFoundException::new);
 
-            //기존 이미지 파일 삭제
+            // 올바른 basePath를 구함
+            String basePath = getItemImgDirectory().toString();
+
+            // 기존 이미지 파일 삭제
             if (!StringUtils.isEmpty(savedItemThumbnail.getImgName())) {
-                fileService.deleteFile(itemImgLocation + "/" +
-                        savedItemThumbnail.getImgName());
+                fileService.deleteFile(basePath + "/" + savedItemThumbnail.getImgName());
             }
 
             String oriImgName = itemThumbnailFile.getOriginalFilename();
-            String imgName = fileService.createThumbnail(itemImgLocation, oriImgName, itemThumbnailFile.getBytes());
+            String imgName = fileService.createThumbnail(basePath, oriImgName, itemThumbnailFile.getBytes());
             String imgUrl = "/images/item/" + imgName;
             savedItemThumbnail.updateItemThumbnail(oriImgName, imgName, imgUrl);
 
             itemThumbnailRepository.save(savedItemThumbnail);
         }
-
     }
     public ItemImg getByItemId(Long itemId){
         return itemImgRepository.findFirstByItemIdOrderByIdAsc(itemId);
