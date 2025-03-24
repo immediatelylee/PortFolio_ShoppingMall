71eeb4673f56325a4a196f659dc65fdca65ae374
lee <dydgh1095@naver.com>
2025-03-25 05:57:54 +0900

    #73-이미지 경로 수정
    기존에는 uploadpath에  생성되는 모든 이미지(itemdetail,itemimg,itemthumbnail)를 넣고 썻다면
    각각의 이미지type에 맞는 디렉토리 하위에 생성될수 있도록 변경하였음.
    그리고 classpath에 있는 이미지 경로와 실제 업로드 해서 하는 경로를 따로 분리하였음.
    
    이제 uploadpath 변수에 /tmp/shop이라고 지정만하면 로컬 aws둘다 사진 upload기능이 적용되고 type에 따라 다르게 디렉토리에 저장된다.

--- src/main/java/com/shoppingmall/project_shoppingmall/config/WebMvcConfig.java
+++ src/main/java/com/shoppingmall/project_shoppingmall/config/WebMvcConfig.java
@@ -11,10 +11,29 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Value("${uploadPath}")
     String uploadPath;
 
+
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/images/**")
-                .addResourceLocations(uploadPath);
+                // classpath의 정적 이미지 제공 (초기 이미지)
+                .addResourceLocations("classpath:/static/images/");
+
+
+        // 업로드된 이미지 제공 (aws에서는 /tmp 폴더 사용)
+        registry.addResourceHandler("/uploaded_images/**")
+                .addResourceLocations(uploadPath); // uploadPath = file:/tmp/shop/
+
+
+        // 업로드된 모든 이미지 제공 (AWS /tmp/shop/ 디렉토리 사용)
+        registry.addResourceHandler("/uploaded_images/itemimg/**")
+                .addResourceLocations(uploadPath + "/itemimg/");
+
+        registry.addResourceHandler("/uploaded_images/itemdetailimg/**")
+                .addResourceLocations(uploadPath + "/itemdetailimg/");
+
+        registry.addResourceHandler("/uploaded_images/itemthumbnail/**")
+                .addResourceLocations(uploadPath + "/itemthumbnail/");
     }
 
 }
