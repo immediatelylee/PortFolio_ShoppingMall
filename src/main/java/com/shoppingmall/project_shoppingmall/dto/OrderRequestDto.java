999c9093289773cfd6d1d4ab0541e860eb528346
lee <dydgh1095@naver.com>
2025-11-22 13:29:49 +0900

    #87 - validation 추가

--- src/main/java/com/shoppingmall/project_shoppingmall/dto/OrderRequestDto.java
+++ src/main/java/com/shoppingmall/project_shoppingmall/dto/OrderRequestDto.java
@@ -2,10 +2,13 @@ package com.shoppingmall.project_shoppingmall.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
 import java.util.*;
 
 @Getter
 @Setter
 public class OrderRequestDto {
+
+    @NotEmpty(message = "주문할 상품이 없습니다.")
     private List<CartItemOrderDto> items;
 }
