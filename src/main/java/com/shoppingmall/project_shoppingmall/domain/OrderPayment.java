47f04f9ebcadc788614e973e955889a34806dc8a
lee <dydgh1095@naver.com>
2025-12-03 13:08:35 +0900

    #91 - 결제 도메인 최신화

--- src/main/java/com/shoppingmall/project_shoppingmall/domain/OrderPayment.java
+++ src/main/java/com/shoppingmall/project_shoppingmall/domain/OrderPayment.java
@@ -1,5 +1,6 @@
 package com.shoppingmall.project_shoppingmall.domain;
 
+import com.shoppingmall.project_shoppingmall.constant.PaymentStatus;
 import lombok.*;
 
 import javax.persistence.*;
@@ -19,13 +20,40 @@ public class OrderPayment {
     private BigDecimal amount; // amount 필드를 BigDecimal로 변경
     private LocalDateTime paymentDate;
 
+    private String payKey;       // PG에서 내려주는 키 (있으면)
+    private String pgTid;        // PG 거래 ID (선택)
+
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+
     @OneToOne
     @JoinColumn(name = "order_id")
     private Order order;
 
-    public OrderPayment(String paymentMethod, BigDecimal amount, LocalDateTime paymentDate) {
+
+
+    @Builder
+    public OrderPayment(Order order,
+                        String paymentMethod,
+                        BigDecimal amount,
+                        LocalDateTime paymentDate,
+                        PaymentStatus paymentStatus,
+                        String payKey,
+                        String pgTid) {
+        this.order = order;
         this.paymentMethod = paymentMethod;
         this.amount = amount;
         this.paymentDate = paymentDate;
+        this.paymentStatus = paymentStatus;
+        this.payKey = payKey;
+        this.pgTid = pgTid;
+    }
+
+    public void markSuccess() {
+        this.paymentStatus = PaymentStatus.SUCCESS;
+    }
+
+    public void markFailed() {
+        this.paymentStatus = PaymentStatus.FAILED;
     }
 }
